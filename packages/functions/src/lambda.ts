66723726546f0754b9fd66b2fe234ad34affcd44
Ankcorn <thomasankcorn@gmail.com>
2023-03-28 10:10:14 +0100

    lambda.ts update

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -2,7 +2,7 @@ import { Time } from "@lambda-node-opentelemetry-serverless-stack-v2/core/time";
 
 const handler = async () => {
   return {
-    body: `Hello world. The time is ${Time.now()}!`,
+    body: `Hello world. The time is ${Time.now()}`,
   };
 };
 
